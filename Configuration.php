13c98460d705e8cddc584568ef98b332ec3c65b3
Pierre-Jean LEGER <leger.pierrejean@orange.fr>
2013-02-19 08:17:31 +0100

    Correction d'un bug lors de la définition des configurations

--- Configuration.php
+++ Configuration.php
@@ -1,7 +1,7 @@
 <?php
 	// Paramètres
-	define("Root", str_replace("router.php", "", $_SERVER['SCRIPT_NAME']));
-	define("Path", str_replace("router.php", "", $_SERVER['SCRIPT_FILENAME']));
+	define("Root", str_replace("Router.php", "", $_SERVER['SCRIPT_NAME']));
+	define("Path", str_replace("Router.php", "", $_SERVER['SCRIPT_FILENAME']));
 	define("Homepage", $_SERVER['SERVER_NAME'].Root);
 	define("DebugMode", "true");
 	define("DBHost", "localhost");
